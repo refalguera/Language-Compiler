1124e77debea88599ff8d60b397f6c3802af620c
Renata Falguera Gonçalves <renata.falguera@gmail.com>
2017-04-26 20:10:36 -0300

    Update tokens.js

--- tokens.js
+++ tokens.js
@@ -1,40 +1,51 @@
-var Tokens = {
+ar Tokens = {
     itens: [],
 
-    addNumero: function(parametro){
+    addNumero: function(parametro,linha,coluna){
         var token = {};
         token.tipo = "numero";
         token.valor = parametro;
+        token.linha = linha;
+        token.coluna = coluna;
         this.itens.push(token);
     },
 
-    addString:function(parametro){
+    addString:function(parametro,linha,coluna){
         var token = {};
         token.tipo = "string";
         token.valor = parametro;
+        token.linha = linha;
+        token.coluna = coluna;
         this.itens.push(token);
     },
 
-    addOperador:function(parametro){
+    addOperador:function(parametro,linha,coluna){
         var token = {};
         token.tipo = "operador";
         token.valor = parametro;
+        token.linha = linha;
+        token.coluna = coluna;
         this.itens.push(token);
     },
 
-    addIdentificador:function(id){
+    addIdentificador:function(id,linha,coluna){
         var token = {};
         token.tipo = "id";
         token.valor = id;
+        token.linha = linha;
+        token.coluna = coluna;
         this.itens.push(token);
     },
 
-    addPontuacao:function(parametro){
+    addPontuacao:function(parametro,linha,coluna){
         var token = {};
         token.tipo = "pontuacao";
         token.valor = parametro;
+        token.linha = linha;
+        token.coluna = coluna;
         this.itens.push(token);
     }
+
 };
 
 module.exports = Tokens;

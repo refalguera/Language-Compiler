a94ceb31c715452f64b26b48f0355b6e327413e6
wentel <victorhugo.dias@uol.com.br>
2017-04-26 20:14:55 -0300

    Update tabela.js

--- tabela.js
+++ tabela.js
@@ -14,7 +14,8 @@ var Tabela = {
 	
 	//Retorna se é palavra reservada do Pascal (Retorna true caso seja, se não, retorna false)
 	ehPascal: function(nomeIdentificador) {
-			var ehp = identificadores.indexOf(nomeIdentificador);
+			nomeIdentificador = nomeIdentificador.toUpperCase();
+			var ehp = this.identificadores.indexOf(nomeIdentificador);
 			if (ehp == -1 || ehp >=50) 
 				return false; 
 				//Se -1 não foi adicionado a tabela ainda, se for maior que 50, o indice se refere a um identificador criado durante execução
@@ -25,10 +26,10 @@ var Tabela = {
 	//Retorna a ID correspondente a um identificador e caso não haja um o mesmo é criado e retornado
 	pegarId: function(nomeIdentificador) {
 			nomeIdentificador = nomeIdentificador.toUpperCase();
-			var i = ehPascal(nomeIdentificador);
+			var i = this.identificadores.indexOf(nomeIdentificador);
 			if (i != -1) return i;  					         	//Retorna a ID
-			identificadores.push(nomeIdentificador);		//Adiciona o identificador
-			return identificadores.length - 1;		  		//Retorna a ID gerada
+			this.identificadores.push(nomeIdentificador);		//Adiciona o identificador
+			return this.identificadores.length - 1;		  		//Retorna a ID gerada
 		}
 }
 
